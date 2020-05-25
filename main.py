b76660ff16f45a9721fd7b2e4dcf8e8d216c3091
Octavia Isom <55517177+octaviaisom@users.noreply.github.com>
2020-05-24 20:47:21 -0400

    all functions, final edits needed

--- main.py
+++ main.py
@@ -216,5 +216,222 @@ def store_plots():
             'yanchor': 'top'})
     
 
-    return store_cluster_scatter
+    return 
+
+#---------------------------------------------------------------------------------------
+
+#Sales by Dept
+def dept_plots():
+    byDept = sales.groupby(['Dept']).sum()
+    byDept.head()
+
+    byDept_bar = go.Figure(data=go.Bar(x=byDept.index,y=byDept.Weekly_Sales,marker_color=walmart_palette[0]))
+    byDept_bar.update_layout(title={
+            'text': "Sales by Dept.",
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
+    
+
+    #Dickey-Fuller Test
+    from statsmodels.tsa.stattools import adfuller
+    dept_w_missing_wks = sales.groupby('Dept').nunique().sort_values('Date').query('Date<143')
+    missing_wks = dept_w_missing_wks.index
+    stat_list = []
+    stat_bool_list = []
+    dept_list = []
+    depts = sales.Dept.unique()
+    for dept in depts:
+        if dept in missing_wks:
+            continue
+            
+        byDeptx = sales.query(f"Dept=={dept}").groupby('Date').sum()
+        dftest = adfuller(byDeptx['Weekly_Sales'], maxlag=55)
+        p_value = dftest[1]
+        lags = dftest[2]
+
+        if p_value <= 0.05:
+            stationarity = "Stationary"
+            station_bool = 1
+            
+        else:
+            stationarity = "Non-Stationary"
+            station_bool = 0
+        
+        dept_list.append(dept)
+        stat_list.append(stationarity)
+        stat_bool_list.append(station_bool)
+    stationarities = pd.DataFrame({'Dept': dept_list,
+                                'Stationary': stat_list,
+                                'Stationary_Bool': stat_bool_list})
+    
+    byStat = pd.merge(sales, stationarities, on='Dept', how='left')
+    byStat = byStat.groupby(['Stationary','Date'], as_index=False).sum()
+
+    byStat_line = px.line(byStat,x='Date', y='Weekly_Sales', color='Stationary',color_discrete_sequence=walmart_palette)
+    byStat_line.update_layout(title={
+            'text': "Department Stationarity - Sales",
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
+    
+    byStat_pie = px.pie(byStat, values='Weekly_Sales', names='Stationary',color_discrete_sequence=walmart_palette)
+    byStat_pie.update_layout(legend_orientation="h",
+                            title={
+            'text': "Department Stationarity - Sales",
+            'y':.95,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
+    
+    return 
+
+#Create Models
+def model_plots():
+    from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+    from pmdarima import auto_arima    
+
+
+    models = byDate[['Weekly_Sales', 'IsHoliday']]
+    models.IsHoliday = byDate.IsHoliday.astype('bool').astype('int')
+    models.head()
+
+    models.index.freq = 'W-FRI'
+    models.index
+
+    #Run pmdarima.auto_arima to obtain recommended orders
+    auto_arima(models['Weekly_Sales'],seasonal=True,m=52).summary()
+
+
+    #Test/Train Split
+    length = len(models)
+    train_len = int(length*0.70)
+
+    train = models.iloc[:train_len]
+    test = models.iloc[train_len:]
+
+
+    #Fit Model 1 (2,0,2)(1,0,0,52)
+    model1 = SARIMAX(train['Weekly_Sales'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
+    results = model1.fit()
+    results.summary()
+
+
+    #Retrieve Predicted Values
+    start=len(train)
+    end=len(train)+len(test)-1
+    predictions1 = results.predict(start=start, end=end, dynamic=False)
+    models['NoHolidays'] = predictions1
+
+    #drop nan values from train set
+    models.dropna(inplace=True)
+    models.head()
+
+    #Evaluate Model 1
+    models_lines = go.Figure()
+
+    models_lines.add_trace(
+        go.Scatter(
+            x=models.index,
+            y=models.Weekly_Sales,
+            name = "Weekly Sales - Actual",
+            line=dict(color=walmart_palette[0],dash='dot'), 
+        ))
+    models_lines.add_trace(
+        go.Scatter(
+            x=models.index,
+            y=models.NoHolidays,
+            name = "Model 1 - No Holidays",
+            line=dict(color=walmart_palette[1])
+        ))
+    models_lines.update_layout(title={
+            'text': "Model Evaluation",
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
+
+    #SARIMAX (Walmart's Holidays)
+    model2 = SARIMAX(train['Weekly_Sales'],exog=train['IsHoliday'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
+    results = model2.fit()
+    results.summary()
+
+    #Retrieve Predicted Values
+    start=len(train)
+    end=len(train)+len(test)-1
+    exog_forecast = test[['IsHoliday']]  # requires two brackets to yield a shape of (35,1)
+    predictions2 = results.predict(start=start, end=end, exog=exog_forecast)
+    models['WalmartHolidays'] = predictions2
+    models.head()
+
+    #Evaluate Model 2
+    models_lines.add_trace(
+        go.Scatter(
+            x=models.index,
+            y=models.WalmartHolidays,
+            name = "Model 2 - Walmart's Holidays",
+            line=dict(color=walmart_palette[2])
+        ))
+    
+
+    #SARIMAX (All Holidays)
+    byDatex = byDate_all
+    
+    length = len(byDatex)
+    train_len = int(length*0.70)
+
+    train = byDatex.iloc[:train_len]
+    test = byDatex.iloc[train_len:]
+
+    model3 = SARIMAX(train['Weekly_Sales'],exog=train['IsHoliday'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
+    results = model3.fit()
+    results.summary()
+
+    start=len(train)
+    end=len(train)+len(test)-1
+    exog_forecast = test[['IsHoliday']]  # requires two brackets to yield a shape of (35,1)
+    predictions3 = results.predict(start=start, end=end, exog=exog_forecast)
+    models['AllHolidays'] = predictions3
+    
+    #Evaluate Model 3
+    models_lines.add_trace(
+        go.Scatter(
+            x=models.index,
+            y=models.AllHolidays,
+            name = "Model 3 - All Holidays",
+            line=dict(color=walmart_palette[5])
+        ))
+   
+    #Forecast the Future
+    byDate = byDate[['Weekly_Sales','IsHoliday']]
+    
+    csv = pd.read_csv("data/fcast.csv")
+    fcast_dates = pd.DataFrame(csv)
+    fcast_dates.IsHoliday = fcast_dates.IsHoliday.astype('bool').astype('int')
+    fcast_dates.Date = pd.to_datetime(fcast_dates.Date)
+    
+    fcast_dates.set_index('Date', inplace=True)
+    fcast_dates.index.freq = 'W-FRI'
+    
+    model2x = SARIMAX(byDate['Weekly_Sales'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
+    results = model2x.fit()
+
+    #Forcast Future Sales
+    exog_forecast = fcast_dates[['IsHoliday']]
+    fcast_predics = results.predict(fcast_dates.index.min(),fcast_dates.index.max(),exog=exog_forecast)
+    fcast_dates['Weekly_Sales'] = fcast_predics
+    
+
+    fcast_dates['SalesType'] = "Forecast"
+    byDate['SalesType'] = "Actual"
+    byDateFcast = pd.concat([byDate,fcast_dates])
+    
+    #Evaluate Forecast
+    fcast_line = px.line(byDateFcast, x=byDateFcast.index,y='Weekly_Sales', color='SalesType',color_discrete_sequence=walmart_palette)
+    
+    return
+
 
