332172c4c0c2494a8c0bee02aef18c14938b5ffe
Octavia Isom <55517177+octaviaisom@users.noreply.github.com>
2020-05-25 21:04:40 -0400

    dash app in progress

--- app.py
+++ app.py
@@ -5,36 +5,29 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.express as px
-#import main
+import main
+from statsmodels.tsa.stattools import adfuller
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
-
-# the style arguments for the sidebar. We use position:fixed and a fixed width
-SIDEBAR_STYLE = {
+sidebar_style = {
     "position": "fixed",
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "25rem",
+    "width": "22rem",
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
 
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-CONTENT_STYLE = {
-    "margin-left": "27rem",
+content_style = {
+    "margin-left": "24rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
 
-#will be retured by each app
-narrative = "A simple sidebar layout with navigation linksA simple sidebar layout with navigation links A simple sidebar layout with navigation linksA simple sidebar layout with navigation links"
-sidebar_header = "Unique Header"
-
-
+#SIDEBAR COMPONENTS
 sidebar = html.Div(
     [
         #dropdown menu
@@ -47,8 +40,8 @@ sidebar = html.Div(
                         dbc.DropdownMenuItem("Overview"),
                         dbc.DropdownMenuItem(divider=True),
 
-                        dbc.DropdownMenuItem("Exploratoy Analysis", header=True),
-                        dbc.DropdownMenuItem("Stores", active=True),
+                        dbc.DropdownMenuItem("Exploratoy Data Analysis", header=True),
+                        dbc.DropdownMenuItem("Stores"),
                         dbc.DropdownMenuItem("Departments"),
                         dbc.DropdownMenuItem(divider=True),
 
@@ -65,10 +58,10 @@ sidebar = html.Div(
         ),
         
         #sidebar header
-        html.H4(sidebar_header, className="display-4"),
+        html.H4("Title Here", className="display-4"),
 
         #sidebar paragraph
-        html.P(narrative, className="lead"),
+        html.P("Lorem ipsum dolor sit amet, consectetur rus semper eget. Amet dictum sit amet justo donec enim diam.", className="lead"),
         
         #progress bar
         html.Div(dbc.Progress(value=50, style={"height": "3px"}, className="mb-3")),
@@ -83,51 +76,126 @@ sidebar = html.Div(
             ]
         )
     ],
-    style=SIDEBAR_STYLE,
+    style=sidebar_style,
 )
 
-content = html.Div(id="page-content", style=CONTENT_STYLE)
 
+
+
+content = html.Div(id="page-content", style=content_style)
 app.layout = html.Div([dcc.Location(id="url"),sidebar, content])
-
-
-# this callback uses the current pathname to set the active state of the
-# corresponding nav link to true, allowing users to tell see page they are on
-@app.callback(
-    [Output(f"page-{i}-link", "active") for i in range(1, 4)],
-    [Input("url", "pathname")],
-)
-
-def toggle_active_links(pathname):
-    if pathname == "/":
-        # Treat page 1 as the homepage / index
-        return True, False, False
-    return [pathname == f"/page-{i}" for i in range(1, 4)]
-
-
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 
 
+
+
 def render_page_content(pathname):
-    if pathname in ["/", "/page-1"]:
+#HOME PAGE
+    if pathname == "/":
+        narrative = ""
+        sidebar_header = "Walmart Sales Analysis"
+        store1 = main.store_plots()[0]
+        store2 = main.store_plots()[1]
+        store3 = main.store_plots()[2]
+        store4 = main.store_plots()[3]
+        dept1 = main.dept_plots()
+        #dept2 = main.dept_plots()[1]
+        model1 = main.model_plots()[0]
+        model2 = main.model_plots()[1]
+        sales = main.sales_plots()
+        #overview content
+        overview = html.Div(
+            [
+                dbc.Row(dbc.Col(html.P("hhh"))),
 
+                dbc.Row(
+                    [
+                        dbc.Col(html.Div(dcc.Graph(figure=model1))),
+                        dbc.Col(html.Div(dcc.Graph(figure=model2))),
+                    ]
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(html.Div(dcc.Graph(figure=sales))),
+                    
+                    ]
+                ),
+            ]
+        )
+        
+        return overview
+
+#EDA OVERVIEW ROUTE
+    elif pathname == "/exp_analysis":
+        narrative = ""
+        sidebar_header = "Exploratory Data Analysis"
+
+
+        
+        return narrative
+
+#STORE PAGE ROUTE
+    elif pathname == "/stores":
+        narrative = ""
+        sidebar_header = "Stores"
+
+        #overview content
         bar = main.store_plots()
-
-        page1 = html.Div([
+        overview = html.Div([
             dcc.Graph(figure=bar)
         ])
-        
-        return page1
-    elif pathname == "/page-2":
-        return html.P("This is the content of page 2. Yay!")
-    elif pathname == "/page-3":
-        return html.P("Oh cool, this is page 3!")
-    # If the user tries to reach a different page, return a 404 message
+
+              
+        return 
+
+#DEPT PAGE ROUTE
+    elif pathname == "/depts":
+        narrative = ""
+        sidebar_header = "Departments"
+
+        #overview content
+        bar = main.store_plots()
+        overview = html.Div([
+            dcc.Graph(figure=bar)
+        ])
+
+              
+        return narrative, sidebar_header, depts
+
+#PRED. MODELING ROUTE
+    elif pathname == "/pred_models":
+        narrative = ""
+        sidebar_header = "Predictive Modeling"
+
+        #overview content
+        bar = main.store_plots()
+        overview = html.Div([
+            dcc.Graph(figure=bar)
+        ])
+
+               
+        return narrative, sidebar_header, pred_models
+
+#CONCLUSION ROUTE
+    elif pathname == "/conclusion":
+        narrative = ""
+        sidebar_header = "Conclusion"
+
+        #overview content
+        bar = main.store_plots()
+        overview = html.Div([
+            dcc.Graph(figure=bar)
+        ])
+
+               
+        return narrative, sidebar_header, conclusion
+
+#404 ROUTE      
     return dbc.Jumbotron(
         [
             html.H1("404: Not found", className="text-danger"),
             html.Hr(),
-            html.P(f"The pathname {pathname} was not recognized..."),
+            html.P(f"The pathname {pathname} was not recognized."),
         ]
     )
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
 
+
 import pandas as pd
 import numpy as np
 import datetime
@@ -29,7 +30,6 @@ for index, row in byDate_all.iterrows():
 
     try:   
         byDate_all.loc[index, "IsHoliday"] = bool(set(week) & set(holidates))
-        print(f"{index}: Holiday Check Complete")
 
     except:
         print(f"{index}: Unable to Complete Holiday Check, Skipping...")
@@ -53,16 +53,24 @@ def sales_plots():
     byDate.IsHoliday = byDate.IsHoliday.astype('bool').astype('int')
     byDate.index.freq = 'W-FRI'
 
+    ymin=byDate.Weekly_Sales.min()
+    ymax=byDate.Weekly_Sales.max()
 
-    byDate_line = go.Figure(data=go.Scatter(x=byDate.index,y=byDate.Weekly_Sales, name='Weekly_Sales',marker_color=walmart_palette[0]))
-    byDate_line.update_layout(title={
-            'text': "Total Weekly Sales",
+    byDate_markers = go.Figure(data=go.Scatter(x=byDate_all.index,y=byDate_all.Weekly_Sales, name='Weekly_Sales',marker_color=walmart_palette[0]))
+    for date in byDate_all.query("IsHoliday==1").index:
+        byDate_markers.add_shape(type="line",
+                    x0=date,
+                    y0=ymin,
+                    x1=date,
+                    y1=ymax,
+                    opacity=0.2,)
+    byDate_markers.update_layout(title={
+            'text': "Total Weekly Sales with All Holiday Markers",
             'y':0.9,
             'x':0.5,
             'xanchor': 'center',
-            'yanchor': 'top'}
-    )
-    byDate_line.update_xaxes(
+            'yanchor': 'top'})
+    byDate_markers.update_xaxes(
         rangeslider_visible=True,
         rangeselector=dict(
             buttons=list([
@@ -73,50 +81,8 @@ def sales_plots():
             ])
         )
     )
-    byDate_line.update_layout(title={
-            'text': "Total Weekly Sales",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
 
-
-    ymin=byDate.Weekly_Sales.min()
-    ymax=byDate.Weekly_Sales.max()
-    byDate_wm_markers = go.Figure(data=go.Scatter(x=byDate.index,y=byDate.Weekly_Sales, name='Weekly_Sales',marker_color=walmart_palette[0]))
-    final_wk = max(byDate.index)
-    for date in byDate.query("IsHoliday==1").index:
-        byDate_wm_markers.add_shape(type="line",
-                    x0=date,
-                    y0=ymin,
-                    x1=date,
-                    y1=ymax,
-                    opacity=0.2)
-    byDate_wm_markers.update_layout(title={
-            'text': "Total Weekly Sales with Walmart's Holiday Markers",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-
-
-    byDate_all_markers = go.Figure(data=go.Scatter(x=byDate_all.index,y=byDate_all.Weekly_Sales, name='Weekly_Sales',marker_color=walmart_palette[0]))
-    for date in byDate_all.query("IsHoliday==1").index:
-        byDate_all_markers.add_shape(type="line",
-                    x0=date,
-                    y0=ymin,
-                    x1=date,
-                    y1=ymax,
-                    opacity=0.2,)
-    byDate_all_markers.update_layout(title={
-            'text': "Total Weekly Sales with All Holiday Markers",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-
-
-    return byDate_line, byDate_wm_markers, byDate_all_markers
+    return byDate_markers
 
 #---------------------------------------------------------------------------------------
 
@@ -126,28 +92,9 @@ def store_plots():
     byStore = pd.merge(byStore, stores, on='Store', how='left')
     byStore = byStore[["Store", "Weekly_Sales", "Type", "Size"]]
 
-    byStore_Bar = go.Figure(data=go.Bar(x=byStore.Store,y=byStore.Weekly_Sales,name='Weekly_Sales',marker_color=walmart_palette[0]))
-    byStore_Bar.update_layout(title={
-            'text': "Total Weekly Sales by Store",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-
-
     size_dist = px.histogram(byStore, x="Size", color='Type', color_discrete_sequence=walmart_palette)
     size_dist.update_layout(title={
             'text': "Size Distribution",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-
-
-    store_sales_scatter = px.scatter(byStore, x="Size",y='Weekly_Sales',color='Type', color_discrete_sequence=walmart_palette)
-    store_sales_scatter.update_traces(marker=dict(size=10, opacity=0.8))
-    store_sales_scatter.update_layout(title={
-            'text': "Size v. Sales",
             'y':0.9,
             'x':0.5,
             'xanchor': 'center',
@@ -167,75 +114,21 @@ def store_plots():
 
     store_type_box = px.box(byStore, x="Type", y="Weekly_Sales", color='Type',color_discrete_sequence=walmart_palette)
     store_type_box.update_traces(width=0.7)
-    store_type_box.update_layout(title={
-            'text': "Weekly Sales by Store Type",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
 
 
     store_size_box = px.box(byStore, x="Type", y="Size", color='Type',color_discrete_sequence=walmart_palette)
     store_size_box.update_traces(width=0.7)
-    store_size_box.update_layout(title={
-            'text': "Total Sq. Ft by Store Type",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
     
 
-    byStorex = byStore[['SalesPerSF','Weekly_Sales', 'Size']]
-
-
-    from sklearn.cluster import KMeans
-    kmeans = KMeans(n_clusters=6) 
-    kmeans.fit(byStorex)
-    byStorex['Cluster'] = kmeans.labels_
-    byStorex = byStorex.sort_values('Cluster')
-    byStorex.Cluster = byStorex.Cluster.astype('object')
-
-
-    store_cluster_scatter = px.scatter(byStorex, x="Size",y='Weekly_Sales',color='Cluster', color_discrete_sequence=walmart_palette)
-    store_cluster_scatter.update_traces(marker=dict(size=10, opacity=0.8))
-    store_cluster_scatter.update_layout(title={
-            'text': "Size v. Sales",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-   
-
-    store_cluster_box = px.box(byStorex, x="Cluster", y="SalesPerSF", color="Cluster",color_discrete_sequence=walmart_palette)
-    store_cluster_box.update_traces(width=.7)
-    store_cluster_box.update_layout(title={
-            'text': "Total Weekly Sales by Store",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-    
-
-    return 
+    return size_dist, store_scatter_sf, store_size_box, store_type_box
 
 #---------------------------------------------------------------------------------------
 
 #Sales by Dept
 def dept_plots():
-    byDept = sales.groupby(['Dept']).sum()
-    byDept.head()
-
-    byDept_bar = go.Figure(data=go.Bar(x=byDept.index,y=byDept.Weekly_Sales,marker_color=walmart_palette[0]))
-    byDept_bar.update_layout(title={
-            'text': "Sales by Dept.",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-    
+    from statsmodels.tsa.stattools import adfuller
 
     #Dickey-Fuller Test
-    from statsmodels.tsa.stattools import adfuller
     dept_w_missing_wks = sales.groupby('Dept').nunique().sort_values('Date').query('Date<143')
     missing_wks = dept_w_missing_wks.index
     stat_list = []
@@ -262,77 +155,53 @@ def dept_plots():
         dept_list.append(dept)
         stat_list.append(stationarity)
         stat_bool_list.append(station_bool)
+
     stationarities = pd.DataFrame({'Dept': dept_list,
                                 'Stationary': stat_list,
                                 'Stationary_Bool': stat_bool_list})
     
+    
     byStat = pd.merge(sales, stationarities, on='Dept', how='left')
+    print(byStat.head())
     byStat = byStat.groupby(['Stationary','Date'], as_index=False).sum()
 
-    byStat_line = px.line(byStat,x='Date', y='Weekly_Sales', color='Stationary',color_discrete_sequence=walmart_palette)
-    byStat_line.update_layout(title={
-            'text': "Department Stationarity - Sales",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-    
+    byStat_line = px.line(byStat,x='Date', y='Weekly_Sales', color='Stationary',color_discrete_sequence=walmart_palette)   
     byStat_pie = px.pie(byStat, values='Weekly_Sales', names='Stationary',color_discrete_sequence=walmart_palette)
-    byStat_pie.update_layout(legend_orientation="h",
-                            title={
-            'text': "Department Stationarity - Sales",
-            'y':.95,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
+
     
-    return 
+    return byStat_line
 
 #Create Models
 def model_plots():
     from statsmodels.tsa.statespace.sarimax import SARIMAX
-
     from pmdarima import auto_arima    
 
-
+    byDate = sales.groupby('Date',sort=False).sum()
     models = byDate[['Weekly_Sales', 'IsHoliday']]
     models.IsHoliday = byDate.IsHoliday.astype('bool').astype('int')
-    models.head()
-
     models.index.freq = 'W-FRI'
-    models.index
-
-    #Run pmdarima.auto_arima to obtain recommended orders
-    auto_arima(models['Weekly_Sales'],seasonal=True,m=52).summary()
-
-
+ 
     #Test/Train Split
     length = len(models)
     train_len = int(length*0.70)
-
     train = models.iloc[:train_len]
     test = models.iloc[train_len:]
 
-
     #Fit Model 1 (2,0,2)(1,0,0,52)
     model1 = SARIMAX(train['Weekly_Sales'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
-    results = model1.fit()
-    results.summary()
-
-
+    results1 = model1.fit()
+    
     #Retrieve Predicted Values
     start=len(train)
     end=len(train)+len(test)-1
-    predictions1 = results.predict(start=start, end=end, dynamic=False)
+    predictions1 = results1.predict(start=start, end=end, dynamic=False)
     models['NoHolidays'] = predictions1
 
     #drop nan values from train set
     models.dropna(inplace=True)
-    models.head()
-
+   
     #Evaluate Model 1
     models_lines = go.Figure()
-
     models_lines.add_trace(
         go.Scatter(
             x=models.index,
@@ -356,17 +225,15 @@ def model_plots():
 
     #SARIMAX (Walmart's Holidays)
     model2 = SARIMAX(train['Weekly_Sales'],exog=train['IsHoliday'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
-    results = model2.fit()
-    results.summary()
-
+    results2 = model2.fit()
+    
     #Retrieve Predicted Values
     start=len(train)
     end=len(train)+len(test)-1
     exog_forecast = test[['IsHoliday']]  # requires two brackets to yield a shape of (35,1)
-    predictions2 = results.predict(start=start, end=end, exog=exog_forecast)
+    predictions2 = results2.predict(start=start, end=end, exog=exog_forecast)
     models['WalmartHolidays'] = predictions2
-    models.head()
-
+   
     #Evaluate Model 2
     models_lines.add_trace(
         go.Scatter(
@@ -376,24 +243,21 @@ def model_plots():
             line=dict(color=walmart_palette[2])
         ))
     
-
     #SARIMAX (All Holidays)
     byDatex = byDate_all
     
     length = len(byDatex)
     train_len = int(length*0.70)
-
     train = byDatex.iloc[:train_len]
     test = byDatex.iloc[train_len:]
 
     model3 = SARIMAX(train['Weekly_Sales'],exog=train['IsHoliday'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
-    results = model3.fit()
-    results.summary()
+    results3 = model3.fit()
 
     start=len(train)
     end=len(train)+len(test)-1
     exog_forecast = test[['IsHoliday']]  # requires two brackets to yield a shape of (35,1)
-    predictions3 = results.predict(start=start, end=end, exog=exog_forecast)
+    predictions3 = results3.predict(start=start, end=end, exog=exog_forecast)
     models['AllHolidays'] = predictions3
     
     #Evaluate Model 3
@@ -407,7 +271,6 @@ def model_plots():
    
     #Forecast the Future
     byDate = byDate[['Weekly_Sales','IsHoliday']]
-    
     csv = pd.read_csv("data/fcast.csv")
     fcast_dates = pd.DataFrame(csv)
     fcast_dates.IsHoliday = fcast_dates.IsHoliday.astype('bool').astype('int')
@@ -417,13 +280,11 @@ def model_plots():
     fcast_dates.index.freq = 'W-FRI'
     
     model2x = SARIMAX(byDate['Weekly_Sales'],order=(2,0,2),seasonal_order=(1,0,0,52),enforce_invertibility=False)
-    results = model2x.fit()
-
+    resultsx = model2x.fit()
     #Forcast Future Sales
     exog_forecast = fcast_dates[['IsHoliday']]
-    fcast_predics = results.predict(fcast_dates.index.min(),fcast_dates.index.max(),exog=exog_forecast)
+    fcast_predics = resultsx.predict(fcast_dates.index.min(),fcast_dates.index.max(),exog=exog_forecast)
     fcast_dates['Weekly_Sales'] = fcast_predics
-    
 
     fcast_dates['SalesType'] = "Forecast"
     byDate['SalesType'] = "Actual"
@@ -432,6 +293,5 @@ def model_plots():
     #Evaluate Forecast
     fcast_line = px.line(byDateFcast, x=byDateFcast.index,y='Weekly_Sales', color='SalesType',color_discrete_sequence=walmart_palette)
     
-    return
-
+    return models_lines, fcast_line
 
